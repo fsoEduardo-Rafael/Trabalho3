547961d90ced72be9219862890804e2fdcc9fe6b
Rafael Fazzolino <fazzolino29@gmail.com>
2016-09-28 00:39:22 -0300

    Questão 2 concluída

---
+++ libprimo/dinamica/lib/primo.h
@@ -0,0 +1,17 @@
+/*
+	libprimo.h
+	Biblioteca utilizada para geração e validação de números primos. 
+	Contempla o trabalho da disciplina de Fundamentos de Sistemas Operacionais - UnB Gama.
+
+	Neste arquivo estão contidos apenas assinaturas das funções existentes na biblioteca.
+
+*/
+
+#ifndef _PRIMO_LIB_
+#define _PRIMO_LIB_
+
+int gera_primo();
+
+int testa_primo(int primo);
+
+#endif // _PRIMO_LIB_
